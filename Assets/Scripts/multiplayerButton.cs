b7f8b15d63e3d63a64b2d3232565a44489093098
davehards <dave@grantoo.org>
2014-10-05 14:02:39 -0700

    Added multiplayerButton.cs

---
+++ Assets/Scripts/multiplayerButton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class multiplayerButton : MonoBehaviour 
+{
+	
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+		
+	}
+	
+	void OnMouseOver () 
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+		}
+	}
+	
+}
