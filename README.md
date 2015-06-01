278cf0cde1e7463848a65ae1853f9ddcd7150b52
unstablesun <unstablesun@gmail.com>
2015-06-01 15:57:11 -0700

    Updated bundleID com.grantoo.tapgear.  Unity folder param for buildTarget.py.  Archived push certs

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ FuelTapGear-Sample-Unity
 ========================
 
 Sample Unity Game
+
+Example of building TapGear from the command line.  
+
+Use the scripts/buildTarget.py script
+
+Parameter List: 
+SKU - ex. Android, iOS
+UnityFolder - ex. Unity or UnityLatest
+TargetName - ex. TapGearVersion1.apk
+UnityProjectPath - and absolute path + project folder
+
+example:
+python buildTarget.py Android Unity tapgear_v2.apk /Users/davehards/Dev/src/FuelTapGear-Sample-Unity
+
--- scripts/buildTarget.py
+++ scripts/buildTarget.py
@@ -7,33 +7,40 @@ import fileinput
 import time
 
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
     print 'arg 1 = Target Build Sku'
-    print 'arg 2 = Output File Name'
-    print 'arg 3 = Unity Project Path'
+    print 'arg 2 = Unity folder name'
+    print 'arg 3 = Output File Name'
+    print 'arg 4 = Unity Project Path'
+    print 'Example:'
+    print 'python buildTarget.py Android Unity TapGear_v1.apk TapGearProjectOath'
     sys.exit(1)
 
 
-#This system only works with Unity 5.x.x
-
+#This system only works with Unity 5.x.x - 4.x.x does not permit building from the command line
 targetBuildSku = sys.argv[1] 	#build target - iOS, Android
-outputFileName = sys.argv[2] 	#name of the output file
-unityProjectPath = sys.argv[3] 	#this is our project that we are building from
+unityFolderName = sys.argv[2] 	#name of the unity folder in applications
+outputFileName = sys.argv[3] 	#name of the output file
+unityProjectPath = sys.argv[4] 	#this is our project that we are building from
 
 
 print 'Target Build Sku : ' + targetBuildSku
+print 'Unity Folder Name : ' + unityFolderName
 print 'Output File Name : ' + outputFileName
 print 'Unity Project Path : ' + unityProjectPath
 
 if targetBuildSku == 'Android':
     print 'building Android...'
-    os.system("/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -projectPath " + unityProjectPath + " -executeMethod AutoBuilder.PerformAndroidBuild " + outputFileName)
+    os.system("/Applications/Unity/Unity.app/Contents/MacOS/" + unityFolderName + " -quit -batchmode -projectPath " + unityProjectPath + " -executeMethod AutoBuilder.PerformAndroidBuild " + outputFileName)
+    print 'Build Complete'
 elif targetBuildSku == 'iOS':
     print 'building iOS...'
-    os.system("/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -projectPath " + unityProjectPath + " -executeMethod AutoBuilder.PerformiOSBuild " + outputFileName)
+    os.system("/Applications/Unity/Unity.app/Contents/MacOS/" + unityFolderName + " -quit -batchmode -projectPath " + unityProjectPath + " -executeMethod AutoBuilder.PerformiOSBuild " + outputFileName)
+    print 'Build Complete'
 else:
     print targetBuildSku + ' not defined'
 
 
-print 'Operation Complete'
+print 'cleaning up...'
+print 'done.'
 
