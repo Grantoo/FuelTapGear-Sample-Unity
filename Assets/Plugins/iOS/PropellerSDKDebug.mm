b0fc9de807ccd5aeaaff19e6017817dcbd6fe630
Jan-Paul Azucena <jp@fuelpowered.com>
2015-08-26 17:36:45 -0700

    [BUGFIX] Using the correct variables for specifying the game key and game secret overriding values

--- Assets/Plugins/iOS/PropellerSDKDebug.mm
+++ Assets/Plugins/iOS/PropellerSDKDebug.mm
@@ -32,8 +32,6 @@ extern "C"
 
     + (void) initializeDebug:(NSString*)gameKey gameSecret:(NSString*)gameSecret gameHasLogin:(BOOL)hasLogin gameHasInvite:(BOOL)hasInvite gameHasShare:(BOOL)hasShare
     {
-        NSString *gameID = nil;
-        NSString *gameKey = nil;
         NSString *sdkUrl = nil;
         NSString *fuelHost = nil;
         NSString *tournamentHost = nil;
@@ -45,8 +43,8 @@ extern "C"
         NSString *env = NSStringize(PSDK_ENV);
         
         if ([env isEqualToString:@"Internal"]) {
-            gameID = @"5522ed09663330000b090000";
-            gameKey = @"62363bb2-cecc-a2bc-694c-5e5e532a214f";
+            gameKey = @"5522ed09663330000b090000";
+            gameSecret = @"62363bb2-cecc-a2bc-694c-5e5e532a214f";
             sdkUrl = @"https://api-internal.fuelpowered.com/sdk/";
             fuelHost = @"https://api-internal.fuelpowered.com/api/v1";
             tournamentHost = @"https://api-internal.fuelpowered.com/api/v1";
@@ -55,8 +53,8 @@ extern "C"
             transactionHost = @"https://transaction-internal.fuelpowered.com/api";
             dynamicsHost = @"http://apiv2-internal.fuelpowered.com/api/v2";
         } else if ([env isEqualToString:@"Sandbox"]) {
-            gameID = @"5477a6c17061701423190000";
-            gameKey = @"bc8939af-ac67-de48-7791-a4dc76ac3cfe";
+            gameKey = @"5477a6c17061701423190000";
+            gameSecret = @"bc8939af-ac67-de48-7791-a4dc76ac3cfe";
             sdkUrl = @"https://api-sandbox.fuelpowered.com/sdk/";
             fuelHost = @"https://api-sandbox.fuelpowered.com/api/v1";
             tournamentHost = @"https://api-sandbox.fuelpowered.com/api/v1";
@@ -65,8 +63,8 @@ extern "C"
             transactionHost = @"https://transaction-sandbox.fuelpowered.com/api";
             dynamicsHost = @"https://api-sandbox.fuelpowered.com/api/v2";
         } else {
-            gameID = @"542b3bec636f62427de7ac00";
-            gameKey = @"7944a1a9-fc41-8789-4c67-dc98bb4c1743";
+            gameKey = @"542b3bec636f62427de7ac00";
+            gameSecret = @"7944a1a9-fc41-8789-4c67-dc98bb4c1743";
             sdkUrl = @"https://api.fuelpowered.com/sdk/";
             fuelHost = @"https://api.fuelpowered.com/api/v1";
             tournamentHost = @"https://api.fuelpowered.com/api/v1";
