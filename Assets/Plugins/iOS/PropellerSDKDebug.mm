1c2419ae00f7c0f863c2967f484a637023fb2f3b
Jan-Paul Azucena <jp@fuelpowered.com>
2015-10-30 16:21:24 -0700

    [REFACTOR] Refactored the mechanism for specifying different target Fuel environments to be simpler and support both Android and iOS platforms

--- Assets/Plugins/iOS/PropellerSDKDebug.mm
+++ Assets/Plugins/iOS/PropellerSDKDebug.mm
@@ -1,108 +1,30 @@
-#ifdef PSDK_DEBUG
-
-#import <objc/runtime.h>
-
 #import "PropellerSDK.h"
-
-#define NSStringize_helper(x) #x
-#define NSStringize(x) @NSStringize_helper(x)
 
 extern "C"
 {
-    static void swizzleClassMethod(Class cls, SEL sourceSelector, SEL targetSelector);
-   
     @interface PropellerSDK (Private)
 
         + (void)useDebugServers:(NSString *)sdkURL fuelAPIURL:(NSString *)fuelAPIURL tournamentAPIURL:(NSString *)tournamentAPIURL challengeAPIURL:(NSString *)challengeAPIURL cdnAPIURL:(NSString *)cdnAPIURL transactionAPIURL:(NSString*)transactionAPIURL dynamicsAPIURL:(NSString*)dynamicsAPIURL;
 
     @end
     
-    @implementation PropellerSDK (Private)
-
-    + (void)load
+    void iOSUseDebugServers(const char* sdkHost, const char* apiHost, const char* tournamentHost, const char* challengeHost, const char* cdnHost, const char* transactionHost, const char* dynamicsHost)
     {
-        static dispatch_once_t onceToken;
-        dispatch_once(&onceToken, ^{
-            swizzleClassMethod(
-                               [PropellerSDK class],
-                               @selector(initialize:gameSecret:gameHasLogin:gameHasInvite:gameHasShare:),
-                               @selector(initializeDebug:gameSecret:gameHasLogin:gameHasInvite:gameHasShare:));
-        });
+        NSString* sdkURL = [NSString stringWithFormat:@"%s", sdkHost];
+        NSString* fuelAPIURL = [NSString stringWithFormat:@"%s", apiHost];
+        NSString* tournamentAPIURL = [NSString stringWithFormat:@"%s", tournamentHost];
+        NSString* challengeAPIURL = [NSString stringWithFormat:@"%s", challengeHost];
+        NSString* cdnAPIURL = [NSString stringWithFormat:@"%s", cdnHost];
+        NSString* transactionAPIURL = [NSString stringWithFormat:@"%s", transactionHost];
+        NSString* dynamicsAPIURL = [NSString stringWithFormat:@"%s", dynamicsHost];
+        
+        [PropellerSDK useDebugServers:sdkURL
+                           fuelAPIURL:fuelAPIURL
+                     tournamentAPIURL:tournamentAPIURL
+                      challengeAPIURL:challengeAPIURL
+                            cdnAPIURL:cdnAPIURL
+                    transactionAPIURL:transactionAPIURL
+                       dynamicsAPIURL:dynamicsAPIURL];
     }
-
-    + (void) initializeDebug:(NSString*)gameKey gameSecret:(NSString*)gameSecret gameHasLogin:(BOOL)hasLogin gameHasInvite:(BOOL)hasInvite gameHasShare:(BOOL)hasShare
-    {
-        NSString *sdkUrl = nil;
-        NSString *fuelHost = nil;
-        NSString *tournamentHost = nil;
-        NSString *challengeHost = nil;
-        NSString *cdnHost = nil;
-        NSString *transactionHost = nil;
-        NSString *dynamicsHost = nil;
-        
-        NSString *env = NSStringize(PSDK_ENV);
-        
-        if ([env isEqualToString:@"Internal"]) {
-            gameKey = @"5522ed09663330000b090000";
-            gameSecret = @"62363bb2-cecc-a2bc-694c-5e5e532a214f";
-            sdkUrl = @"https://api-internal.fuelpowered.com/sdk/";
-            fuelHost = @"https://api-internal.fuelpowered.com/api/v1";
-            tournamentHost = @"https://api-internal.fuelpowered.com/api/v1";
-            challengeHost = @"https://challenge-internal.fuelpowered.com/v1";
-            cdnHost = @"http://cdn-internal.fuelpowered.com/api/v1";
-            transactionHost = @"https://transaction-internal.fuelpowered.com/api";
-            dynamicsHost = @"http://apiv2-internal.fuelpowered.com/api/v2";
-        } else if ([env isEqualToString:@"Sandbox"]) {
-            gameKey = @"5477a6c17061701423190000";
-            gameSecret = @"bc8939af-ac67-de48-7791-a4dc76ac3cfe";
-            sdkUrl = @"https://api-sandbox.fuelpowered.com/sdk/";
-            fuelHost = @"https://api-sandbox.fuelpowered.com/api/v1";
-            tournamentHost = @"https://api-sandbox.fuelpowered.com/api/v1";
-            challengeHost = @"https://challenge-sandbox.fuelpowered.com/v1";
-            cdnHost = @"http://cdn-sandbox.fuelpowered.com/api/v1";
-            transactionHost = @"https://transaction-sandbox.fuelpowered.com/api";
-            dynamicsHost = @"https://api-sandbox.fuelpowered.com/api/v2";
-        } else {
-            gameKey = @"542b3bec636f62427de7ac00";
-            gameSecret = @"7944a1a9-fc41-8789-4c67-dc98bb4c1743";
-            sdkUrl = @"https://api.fuelpowered.com/sdk/";
-            fuelHost = @"https://api.fuelpowered.com/api/v1";
-            tournamentHost = @"https://api.fuelpowered.com/api/v1";
-            challengeHost = @"https://challenge.fuelpowered.com/v1";
-            cdnHost = @"http://cdn.fuelpowered.com/api/v1";
-            transactionHost = @"https://transaction.fuelpowered.com/api";
-            dynamicsHost = @"https://api.fuelpowered.com/api/v2";
-        }
-
-        [PropellerSDK useDebugServers:sdkUrl
-                           fuelAPIURL:fuelHost
-                     tournamentAPIURL:tournamentHost
-                      challengeAPIURL:challengeHost
-                            cdnAPIURL:cdnHost
-                    transactionAPIURL:transactionHost
-                       dynamicsAPIURL:dynamicsHost];
-        
-        [PropellerSDK initializeDebug:gameKey
-                           gameSecret:gameSecret
-                         gameHasLogin:hasLogin
-                        gameHasInvite:hasInvite
-                         gameHasShare:hasShare];
-    }
-
-    @end
     
-    void swizzleClassMethod(Class cls, SEL sourceSelector, SEL targetSelector)
-    {
-        Method sourceMethod = class_getClassMethod(cls, sourceSelector);
-        Method targetMethod = class_getClassMethod(cls, targetSelector);
-        
-        cls = object_getClass((id)cls);
-        
-        if (class_addMethod(cls, sourceSelector, method_getImplementation(targetMethod), method_getTypeEncoding(targetMethod))) {
-            class_replaceMethod(cls, targetSelector, method_getImplementation(sourceMethod), method_getTypeEncoding(sourceMethod));
-        } else {
-            method_exchangeImplementations(sourceMethod, targetMethod);
-        }
-    }
 }
-#endif
---
+++ Assets/Scripts/PropellerSDKDebug.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class PropellerSDKDebug : MonoBehaviour {
+
+#if PROPELLER_SDK_DEBUG
+
+#if UNITY_IPHONE
+	[DllImport ("__Internal")]
+	private static extern void iOSUseDebugServers(string sdkHost, string apiHost, string tournamentHost, string challengeHost, string cdnHost, string transactionHost, string dynamicsHost);
+#elif UNITY_ANDROID
+	private AndroidJavaClass _jniPropellerSDK;
+#endif
+
+	private string _gameKey;
+	private string _gameSecret;
+	private string _sdkHost;
+	private string _apiHost;
+	private string _tournamentHost;
+	private string _challengeHost;
+	private string _cdnHost;
+	private string _transactionHost;
+	private string _dynamicsHost;
+	private string _androidGCMSenderID;
+
+	void Awake ()
+	{
+#if UNITY_ANDROID
+		_jniPropellerSDK = new AndroidJavaClass( "com.fuelpowered.lib.propeller.PropellerSDK" );
+#endif
+
+#if PROPELLER_SDK_DEBUG_INTERNAL
+		_gameKey = "5522ed09663330000b090000";
+		_gameSecret = "62363bb2-cecc-a2bc-694c-5e5e532a214f";
+		_sdkHost = "https://api-internal.fuelpowered.com/sdk/";
+		_apiHost = "https://api-internal.fuelpowered.com/api/v1";
+		_tournamentHost = "https://api-internal.fuelpowered.com/api/v1";
+		_challengeHost = "https://challenge-internal.fuelpowered.com/v1";
+		_cdnHost = "http://cdn-internal.fuelpowered.com/api/v1";
+		_transactionHost = "https://transaction-internal.fuelpowered.com/api";
+		_dynamicsHost = "http://apiv2-internal.fuelpowered.com/api/v2";
+		_androidGCMSenderID = "630730529138";
+#elif PROPELLER_SDK_DEBUG_SANDBOX
+		_gameKey = "5477a6c17061701423190000";
+		_gameSecret = "bc8939af-ac67-de48-7791-a4dc76ac3cfe";
+		_sdkHost = "https://api-sandbox.fuelpowered.com/sdk/";
+		_apiHost = "https://api-sandbox.fuelpowered.com/api/v1";
+		_tournamentHost = "https://api-sandbox.fuelpowered.com/api/v1";
+		_challengeHost = "https://challenge-sandbox.fuelpowered.com/v1";
+		_cdnHost = "http://cdn-sandbox.fuelpowered.com/api/v1";
+		_transactionHost = "https://transaction-sandbox.fuelpowered.com/api";
+		_dynamicsHost = "https://api-sandbox.fuelpowered.com/api/v2";
+		_androidGCMSenderID = "630730529138";
+#elif PROPELLER_SDK_DEBUG_PRODUCTION
+		_gameKey = "542b3bec636f62427de7ac00";
+		_gameSecret = "7944a1a9-fc41-8789-4c67-dc98bb4c1743";
+		_sdkHost = "https://api.fuelpowered.com/sdk/";
+		_apiHost = "https://api.fuelpowered.com/api/v1";
+		_tournamentHost = "https://api.fuelpowered.com/api/v1";
+		_challengeHost = "https://challenge.fuelpowered.com/v1";
+		_cdnHost = "http://cdn.fuelpowered.com/api/v1";
+		_transactionHost = "https://transaction.fuelpowered.com/api";
+		_dynamicsHost = "https://api.fuelpowered.com/api/v2";
+		_androidGCMSenderID = "630730529138";
+#else
+		return;
+#endif
+
+		UpdatePropellerSDKPrefab ();
+		UseDebugServers ();
+	}
+
+	PropellerSDK GetPropellerSDK ()
+	{
+		GameObject gameObject = GameObject.Find("PropellerSDK");
+
+		if (gameObject == null) {
+			throw new Exception("Unable to find the Propeller SDK game object");
+		}
+
+		PropellerSDK propellerSDK = gameObject.GetComponent<PropellerSDK> ();
+
+		if (propellerSDK == null) {
+			throw new Exception("Unable to obtain the Propeller SDK script");
+		}
+
+		return propellerSDK;
+	}
+
+	void UpdatePropellerSDKPrefab ()
+	{
+		PropellerSDK propellerSDK = GetPropellerSDK ();
+		propellerSDK.GameKey = _gameKey;
+		propellerSDK.GameSecret = _gameSecret;
+		propellerSDK.AndroidGCMSenderID = _androidGCMSenderID;
+	}
+
+	void UseDebugServers ()
+	{
+#if UNITY_IPHONE
+		iOSUseDebugServers (_sdkHost, _apiHost, _tournamentHost, _challengeHost, _cdnHost, _transactionHost, _dynamicsHost);
+#elif UNITY_ANDROID
+		_jniPropellerSDK.CallStatic ("useDebugServers", _sdkHost, _apiHost, _tournamentHost, _challengeHost, _cdnHost, _transactionHost, _dynamicsHost);
+#endif
+	}
+
+#endif
+}
